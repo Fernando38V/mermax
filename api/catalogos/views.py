487af10d5eea2556497de79083747a58173b94d6
axelislas <acelislas6889@gmail.com>
2026-07-22 02:12:51 -0700

    Serializers de catalogos iniciado

--- api/catalogos/views.py
+++ api/catalogos/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from catalogos import serializers, models
 
 # Create your views here.
+
+# Empresa Recicladora View Service
+
+class ListRecicladoraAPIView(APIView):
+    
+    def get(self, request):
+        queryset = models.EmpresaRecicladora.objects.all()
+        data = serializers.ListRecicladoraSerializer(queryset, many=True).data
+        return Response(data)
+
+class CreateRecicladoraAPIView(generics.CreateAPIView):
+    queryset = models.EmpresaRecicladora.objects.all()
+    serializer_class = serializers.CreateRecicladoraSerializer
+    
+class DetailRecicladoraAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.EmpresaRecicladora.objects.all()
+    serializer_class = serializers.DetailRecicladoraSerializer
+    
+class UpdateRecicladoraAPIView(generics.UpdateAPIView):
+    queryset = models.EmpresaRecicladora.objects.all()
+    serializer_class = serializers.UpdateRecicladoraSerializer
