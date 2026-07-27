4fed0fd09de6f8fe6180f99184fa44c5e98e9cb7
Fernando38V <anwarfernando14@gmail.com>
2026-07-26 21:30:27 -0700

    solucion a problema de los filtros para el listado de mermas

--- client/mermas/views.py
+++ client/mermas/views.py
@@ -1,33 +1,34 @@
 from django.shortcuts import render
-from django.views import generic
-import requests
 from django.utils.decorators import method_decorator
+from django.views import generic
 from usuarios.decorators import login_required_api
-# Create your views here.
+from usuarios.wrappers import ApiError, api_get
 
 @method_decorator(login_required_api, name='dispatch')
 class ListMermas(generic.View):
     template_name = 'mermas/list_merma.html'
-    context = {}
-    url_base = 'http://127.0.0.1:8000/api/mermas/registro/list/'
-    response = None
-    
+
     def get(self, request):
         token = request.session.get('api_token')
-        headers = {
-            'Authorization': f'Token {token}'
-        }
-        
-        response = requests.get(
-            url=self.url_base, 
-            headers=headers
-        )
-        
-        self.response = response.json()
-        
-        self.context = {
-            "mermas": self.response
-        }
-        
-        return render(request, self.template_name, self.context)
-        
+
+        try:
+            mermas = api_get('/mermas/registro/list/', token=token)
+        except ApiError:
+            mermas = []
+
+        return render(request, self.template_name, {
+            'mermas': mermas,
+            'lineas': self._catalogo('/catalogos/lineas/', token),
+            'tipos_merma': self._catalogo('/catalogos/tipos-merma/', token),
+            'componentes': self._catalogo('/catalogos/componentes/', token),
+            'estados': self._catalogo('/catalogos/lookup/estados-flujo/', token),
+        })
+
+    def _catalogo(self, ruta, token):
+        try:
+            datos = api_get(ruta, token=token)
+        except ApiError:
+            return []
+        if isinstance(datos, dict):
+            return datos.get('results', [])
+        return datos
