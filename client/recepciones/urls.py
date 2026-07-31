c584459397329dd87ad18d3d967f2d78865b7e2f
Fernando38V <anwarfernando14@gmail.com>
2026-07-30 18:53:41 -0700

    Defino las rutas de bandeja, confirmar y discrepancias del almacenista

---
+++ client/recepciones/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'recepciones'
+
+urlpatterns = [
+    path('bandeja/', views.BandejaRecepcion.as_view(), name='bandeja'),
+    path('confirmar/<str:folio>/', views.ConfirmarRecepcion.as_view(), name='confirmar'),
+    path('discrepancias/', views.DiscrepanciasAbiertas.as_view(), name='discrepancias'),
+    path('discrepancias/resolver/<str:folio>/', views.ResolverDiscrepancia.as_view(), name='resolver_discrepancia'),
+]
